dc897ecec767c9ea2f96316c46f9a3f35e3fd4db
Duong Son <duongson29111997@gmail.com>
2018-12-13 23:05:33 +0700

    Save email of the guest

---
+++ app/Http/Controllers/MailController.php
@@ -0,0 +1,37 @@
+<?php
+
+namespace App\Http\Controllers;
+
+use Illuminate\Http\Request;
+use Validator;
+use App\GuestEmail;
+
+class MailController extends Controller
+{
+    public function setEmail(Request $request) {
+    	if (isset($request->guestEmail)) {
+    		$rule = [
+	    		'guestEmail' => 'email|unique:guest_email,email'
+	    	];
+
+	    	$message = [
+	    		'email' => 'The :attribute must be email.',
+	    		'unique' => 'This email is registered.'
+	    	];
+
+	    	$validator = Validator::make($request->all(), $rule, $message);
+
+	    	if ($validator->fails()) {
+	    		return redirect()->back()->withErrors($validator);
+	    	}
+
+	    	$guestEmail = new GuestEmail;
+	    	$guestEmail->email = $request->guestEmail;
+	    	$guestEmail->save();
+
+	    	return redirect()->back()->with('notification', 'Successfully registered email. Thank you!');
+    	} else {
+    		return redirect()->back()->with('notification', 'Email is empty!!');
+    	}
+    }
+}
