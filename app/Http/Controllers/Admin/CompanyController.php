5f4b78017b211b8747bdd7920d2d4fc1f4e9d2e0
Trungnn <trungnn@rabiloo.com>
2018-12-17 21:49:35 +0700

    fix bug task delete company

--- app/Http/Controllers/Admin/CompanyController.php
+++ app/Http/Controllers/Admin/CompanyController.php
@@ -5,6 +5,7 @@ namespace App\Http\Controllers\Admin;
 use Illuminate\Http\Request;
 use App\Http\Controllers\Controller;
 use App\Company;
+use App\JobSummary;
 
 class CompanyController extends Controller
 {
